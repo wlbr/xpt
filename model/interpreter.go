21395110164fbef34874e21f592b3847155558a8
Michael Wolber <mwolber@gmx.de>
2016-04-19 14:05:23 +0200

    s the commit hook active?

--- model/interpreter.go
+++ model/interpreter.go
@@ -5,26 +5,20 @@ package model
 	a := And(Equals(Variable(7), Variable(6)), Equals(Variable(5), Variable(5)))
 */
 type Expression interface {
-	
-	
-	    Interprete(map[string]Expression) bool
+	Interprete(map[string]Expression) bool
 	//Interprete(Map<String,Expression> variables) bool
 }
 
 type VariableExpression struct {
-	Value int
+	Value bool
 }
 
-func Variable(value int) *VariableExpression {
+func Variable(value bool) *VariableExpression {
 	return &VariableExpression{Value: value}
 }
 
-func (s *VariableExpression) Interprete(variables map[string]Expression) int {
-	return s.Value
-}
-
 func (s *VariableExpression) Interprete(variables map[string]Expression) bool {
-	return true
+	return s.Value
 }
 
 type EqualsExpression struct {
@@ -64,4 +58,33 @@ func And(leftOperand Expression, rightOperand Expression) *AndExpression {
 
 func (s *AndExpression) Interprete(variables map[string]Expression) bool {
 	return s.leftOperand.Interprete(variables) && s.rightOperand.Interprete(variables)
+}
+
+type XorExpression struct {
+	leftOperand  Expression
+	rightOperand Expression
+}
+
+func Xor(leftOperand Expression, rightOperand Expression) *XorExpression {
+	return &XorExpression{leftOperand: leftOperand, rightOperand: rightOperand}
+}
+
+func (s *XorExpression) Interprete(variables map[string]Expression) bool {
+	return s.leftOperand.Interprete(variables) != s.rightOperand.Interprete(variables)
+}
+
+type NotExpression struct {
+	operand Expression
+}
+
+func Not(operand Expression) *NotExpression {
+	return &NotExpression{operand: operand}
+}
+
+func (s *NotExpression) Interprete(variables map[string]Expression) bool {
+	return !s.operand.Interprete(variables)
+}
+
+func (s Fact) Interprete(variables map[string]Expression) bool {
+	return true
 }
--- model/interpreter_test.go
+++ model/interpreter_test.go
@@ -2,7 +2,41 @@ package model
 
 import "testing"
 
-func TestSimpleRule(t *testing.T) {
+func assert(t *testing.T, gold bool, test bool) {
+	if gold != test {
+		t.Fail()
+	}
+}
 
-	a := And(Equals(Variable(7), Variable(6)), Equals(Variable(5), Variable(5)))
+
+
+
+func TestSimpleBoolRule(t *testing.T) {
+	a1 := And(Equals(Variable(true), Variable(true)), Equals(Variable(true), Variable(false)))
+	a2 := And(Equals(Variable(true), Variable(true)), Variable(true))
+	a3 := And(Equals(Variable(true), Variable(true)), Variable(false))
+	o1 := Or(Equals(Variable(true), Variable(true)), Equals(Variable(true), Variable(false)))
+	o2 := Or(Equals(Variable(true), Variable(true)), Variable(false))
+	o3 := Or(Equals(Variable(false), Variable(true)), Variable(false))
+	x1 := Xor(Equals(Variable(true), Variable(true)), Equals(Variable(true), Variable(true)))
+	x2 := Xor(Equals(Variable(false), Variable(true)), Equals(Variable(true), Variable(true)))
+	x3 := Xor(Equals(Variable(false), Variable(false)), Equals(Variable(false), Variable(false)))
+	n1 := Not(Variable(true))
+	n2 := Not(Variable(false))
+	n3 := Not(x3)
+
+	v := make(map[string]Expression)
+
+	assert(t, false, a1.Interprete(v))
+	assert(t, true, a2.Interprete(v))
+	assert(t, false, a3.Interprete(v))
+	assert(t, true, o1.Interprete(v))
+	assert(t, true, o2.Interprete(v))
+	assert(t, false, o3.Interprete(v))
+	assert(t, false, x1.Interprete(v))
+	assert(t, true, x2.Interprete(v))
+	assert(t, false, x3.Interprete(v))
+	assert(t, false, n1.Interprete(v))
+	assert(t, true, n2.Interprete(v))
+	assert(t, true, n3.Interprete(v))
 }
